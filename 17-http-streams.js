64d6adb74b5487a9322c92b0a9ec7c8b10c7a4d4
kalaksanan <nankalaksa@gmail.com>
2021-06-15 19:53:29 +0700

    http streams

--- 17-http-streams.js
+++ 17-http-streams.js
@@ -1,11 +1,11 @@
-const http = require('http')
-const fs = require('fs')
+var http = require('http')
+var fs = require('fs')
 
 http
   .createServer(function (req, res) {
-    // const text = fs.readFileSync('./content/big.txt', 'utf-8')
+    // const text = fs.readFileSync('./content/big.txt', 'utf8')
     // res.end(text)
-    const fileStream = fs.createReadStream('./content/big.txt', 'utf-8')
+    const fileStream = fs.createReadStream('./content/big.txt', 'utf8')
     fileStream.on('open', () => {
       fileStream.pipe(res)
     })
