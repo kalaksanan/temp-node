5da5f66a6a8cbdb4c23f8aba65658d6660b04011
kalaksanan <nankalaksa@gmail.com>
2021-06-15 15:42:11 +0700

    third commit

--- 12-http.js
+++ 12-http.js
@@ -3,8 +3,7 @@ const http = require('http')
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     res.end('welcome to our home page')
-  }
-  if (req.url === '/about') {
+  } else if (req.url === '/about') {
     res.end('here is our short history')
   } else {
     res.end(`
